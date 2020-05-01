cf2039228d2f505061817b8daf428f4b4740e697
Sk Imtiaz Ahmed <imtiaz101325@gmail.com>
2020-05-01 22:38:30 +0600

    Print to standard error

--- src/main.rs
+++ src/main.rs
@@ -7,15 +7,12 @@ fn main() {
     let args: Vec<String> = env::args().collect();
 
     let config = Config::new(&args).unwrap_or_else(|err| {
-        println!("Probelm parsing argument: {}", err);
+        eprintln!("Probelm parsing argument: {}", err);
         process::exit(1);
     });
 
-    println!("Searching for \"{}\"", config.query);
-    println!("Searching in ./{}", config.filename);
-
     if let Err(e) = minigrep6000::run(config) {
-        println!("Application error: {}", e);
+        eprintln!("Application error: {}", e);
 
         process::exit(1);
     };
