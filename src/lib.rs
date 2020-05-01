3cc6b0b59310c14aceae0f470521bb9eda49eca1
Sk Imtiaz Ahmed <imtiaz101325@gmail.com>
2020-05-01 21:21:40 +0600

    Implemented search and tested code

--- src/lib.rs
+++ src/lib.rs
@@ -1,14 +1,6 @@
 use std::fs;
 use std::error::Error;
 
-pub fn run(config: Config) -> Result<(), Box<dyn Error>> {
-  let content = fs::read_to_string(config.filename)?;
-
-  println!("File contains:\n{}", content);
-
-  Ok(())
-}
-
 pub struct Config {
   pub query: String,
   pub filename: String,
@@ -27,5 +19,43 @@ impl Config {
           query,
           filename,
       })
+  }
+}
+
+pub fn run(config: Config) -> Result<(), Box<dyn Error>> {
+  let content = fs::read_to_string(config.filename)?;
+
+  for line in search(&config.query, &content) {
+    println!("{}", line);
+  }
+
+  Ok(())
+}
+
+pub fn search<'a>(query: &str, contents: &'a str) -> Vec<&'a str> {
+  let mut results = Vec::new();
+
+  for line in contents.lines() {
+    if line.contains(query) {
+      results.push(line)
+    }
+  }
+  
+  results
+}
+
+#[cfg(test)]
+mod test {
+  use super::*;
+  
+  #[test]
+  fn one_result() {
+    let query = "duct";
+    let contents = "\
+Rust:
+safe, fast, productive.
+Pick three.";
+
+    assert_eq!(vec!["safe, fast, productive."], search(query, contents))
   }
 }
