dcd0ae1640c1954506e16835d85e07e13d57e7c1
Sk Imtiaz Ahmed <imtiaz101325@gmail.com>
2020-05-01 21:52:05 +0600

    Case insensitive search

--- src/lib.rs
+++ src/lib.rs
@@ -44,12 +44,25 @@ pub fn search<'a>(query: &str, contents: &'a str) -> Vec<&'a str> {
   results
 }
 
+pub fn search_case_insensitive<'a>(query: &str, contents: &'a str) -> Vec<&'a str> {
+  let mut results = Vec::new();
+  let query = query.to_lowercase();
+
+  for line in contents.lines() {
+    if line.to_lowercase().contains(&query) {
+      results.push(line)
+    }
+  }
+  
+  results
+}
+
 #[cfg(test)]
 mod test {
   use super::*;
   
   #[test]
-  fn one_result() {
+  fn case_sensitive() {
     let query = "duct";
     let contents = "\
 Rust:
@@ -57,5 +70,17 @@ safe, fast, productive.
 Pick three.";
 
     assert_eq!(vec!["safe, fast, productive."], search(query, contents))
+  }
+
+  #[test]
+  fn case_insensitive() {
+    let query = "rUsT";
+    let contents = "\
+Rust:
+safe, fast, productive.
+Pick three.
+Trust me.";
+
+    assert_eq!(vec!["Rust:", "Trust me."], search_case_insensitive(query, contents))
   }
 }
