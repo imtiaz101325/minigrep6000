edcd16391b36c75e9aad9e9c86620919f893289f
Sk Imtiaz Ahmed <imtiaz101325@gmail.com>
2020-05-02 01:27:53 +0600

    Refactored using iterators

--- src/lib.rs
+++ src/lib.rs
@@ -9,21 +9,25 @@ pub struct Config {
 }
 
 impl Config {
-  pub fn new(args: &[String]) -> Result<Config, &'static str> {
-      if args.len() < 3 {
-          return Err("not enough arguments");
-      }
+  pub fn new(mut args: env::Args) -> Result<Config, &'static str> {
+    args.next();
 
-      let query = args[1].clone();
-      let filename = args[2].clone();
-      
-      let case_sensitive = env::var("CASE_INSENSITIVE").is_err();
+    let query = match args.next() {
+      Some(arg) => arg,
+      None => return Err("Did not recieve a query string.")
+    };
+    let filename = match args.next() {
+      Some(args) => args,
+      None => return Err("Did not recieve filename parameter.")
+    };
+    
+    let case_sensitive = env::var("CASE_INSENSITIVE").is_err();
 
-      Ok(Config {
-          query,
-          filename,
-          case_sensitive,
-      })
+    Ok(Config {
+        query,
+        filename,
+        case_sensitive,
+    })
   }
 }
 
@@ -44,28 +48,19 @@ pub fn run(config: Config) -> Result<(), Box<dyn Error>> {
 }
 
 pub fn search<'a>(query: &str, contents: &'a str) -> Vec<&'a str> {
-  let mut results = Vec::new();
-
-  for line in contents.lines() {
-    if line.contains(query) {
-      results.push(line)
-    }
-  }
-  
-  results
+  contents
+    .lines()
+    .filter(|line| line.contains(query))
+    .collect()
 }
 
 pub fn search_case_insensitive<'a>(query: &str, contents: &'a str) -> Vec<&'a str> {
-  let mut results = Vec::new();
-  let query = query.to_lowercase();
+  let lowercase_query = query.to_lowercase();
 
-  for line in contents.lines() {
-    if line.to_lowercase().contains(&query) {
-      results.push(line)
-    }
-  }
-  
-  results
+  contents
+    .lines()
+    .filter(|line| line.to_lowercase().contains(&lowercase_query))
+    .collect()
 }
 
 #[cfg(test)]
--- src/main.rs
+++ src/main.rs
@@ -4,9 +4,7 @@ use std::process;
 use minigrep6000::Config;
 
 fn main() {
-    let args: Vec<String> = env::args().collect();
-
-    let config = Config::new(&args).unwrap_or_else(|err| {
+    let config = Config::new(env::args()).unwrap_or_else(|err| {
         eprintln!("Probelm parsing argument: {}", err);
         process::exit(1);
     });
