61d2862df6fb8cf1f0608ecf6fb8a80242f46bec
Sk Imtiaz Ahmed <imtiaz101325@gmail.com>
2020-05-01 22:00:54 +0600

    Parsing env var

--- src/lib.rs
+++ src/lib.rs
@@ -1,9 +1,11 @@
 use std::fs;
 use std::error::Error;
+use std::env;
 
 pub struct Config {
   pub query: String,
   pub filename: String,
+  pub case_sensitive: bool,
 }
 
 impl Config {
@@ -14,10 +16,13 @@ impl Config {
 
       let query = args[1].clone();
       let filename = args[2].clone();
-  
+      
+      let case_sensitive = env::var("CASE_INSENSITIVE").is_err();
+
       Ok(Config {
           query,
           filename,
+          case_sensitive,
       })
   }
 }
@@ -25,7 +30,13 @@ impl Config {
 pub fn run(config: Config) -> Result<(), Box<dyn Error>> {
   let content = fs::read_to_string(config.filename)?;
 
-  for line in search(&config.query, &content) {
+  let results = if config.case_sensitive {
+    search(&config.query, &content)
+  } else {
+    search_case_insensitive(&config.query, &content)
+  };
+
+  for line in results {
     println!("{}", line);
   }
 
