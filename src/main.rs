03bc66ff44aef6f213b180eb05a57e84cc744c01
Sk Imtiaz Ahmed <imtiaz101325@gmail.com>
2020-05-01 14:02:23 +0600

    Reading args and file

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,17 @@
+use std::env;
+use std::fs;
+
 fn main() {
-    println!("Hello, world!");
+    let args: Vec<String> = env::args().collect();
+
+    let query = &args[1];
+    let filename = &args[2];
+
+    println!("Searching for \"{}\"", query);
+    println!("Searching in ./{}", filename);
+
+    let content = fs::read_to_string(filename)
+        .expect("Something went wrong while trying to read the file");
+
+    println!("File contains:\n{}", content);
 }
